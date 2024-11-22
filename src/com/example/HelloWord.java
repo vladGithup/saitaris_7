990e41f8f93f2628294d820e32d8597cf0d9bd5e
Vlad Drozdovich <fwfwqfdwqf@gmail.com>
2024-11-22 21:46:44 +0300

    12

--- src/com/example/HelloWord.java
+++ src/com/example/HelloWord.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class HelloWord {
     public static void main(String[] args) {
-
+        System.out.println("NEw lab saitaris");
         System.out.println("Hello, World!");
     }
 }
